c7fa8e0d2e43b40aeb0c0471fa86b694a03a4b97
zengyang0428 <zengyang0428>
2022-05-22 22:50:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 第一个智能合约
+非常的开心
+
+# 安装
+使用纱线安装更为有效
+yarn install
+
+# 运行
+yarn start
+
+# 使用说明
+这个项目是使用React开发的，运行之后在Chrome浏览器中打开地址
+http://localhost:3000  请确认你安装了Metamask插件
+
---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Web3 from "web3";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value : 0
+    };
+  }
+  async componentDidMount() {
+    //判断页面是否安装Metamask
+    if (typeof window.ethereum !== 'undefined') {
+      const ethereum = window.ethereum
+      //禁止自动刷新，metamask要求写的
+      //自动刷新连接更改
+      ethereum.autoRefreshOnNetworkChange = false
+
+      try {
+        //第一次链接Metamask
+        const accounts = await ethereum.enable()
+        console.log(accounts)
+        //初始化Provider 
+        const provider = window['ethereum']
+        console.log(provider)
+        //获取网络ID
+        console.log(provider.chainId)
+        //实例化Web3
+        const web3 = new Web3(provider)
+        console.log(web3)
+        //导入abi文件
+        const abi = require("./hee.bai.json")
+        //定义合约地址
+        const address = "0x4bad726d6FfBCbED2F44e38a40EA3798966Cc6cE"
+        //实例化合约
+        window.myContract = new web3.eth.Contract(abi.abi,address)
+        console.log(window.myContract)
+        //获取当前账户地址
+        window.defaultAccount = accounts[0];
+        console.log(window.defaultAccount)
+        //帐户更改
+        ethereum.on('accountsChanged', function (accounts) {
+          console.log("accountsChanged:" + accounts)
+        })
+        //网络已更改
+        ethereum.on('networkChanged', function (networkVersion) {
+          console.log("networkChanged:" + networkVersion)
+        })
+      } catch (e) {
+
+      }
+    } else {
+      console.log('没有安装metamask')
+    }
+  }
+   Getter = () => {
+     //then 应该是获取当前的value
+    window.myContract.methods.value().call().then(value=>{
+      //打印value
+      console.log(value)
+      //传入
+      this.setState({value:value})
+    })
+  }
+  Increase = () => {
+    //increaase 是智能合约中的函数 send可以放入两个参数 第一个地址 第二个gas
+    window.myContract.methods.increaase(123).send({from:window.defaultAccount})
+    .on('transactionHash',(transactionHash)=>{
+      console.log('transactionHash',transactionHash)
+    })
+    
+  }
+  render() {
+    return (
+      <div>
+        <div>{this.state.value}</div>
+        <div>
+          <button onClick={() => { this.Getter() }}>Getter</button>
+        </div>
+        <div>
+          <button onClick={() => { this.Increase() }}>Increase</button>
+        </div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default App;
